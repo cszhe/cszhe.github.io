b42c70aca10724ec8f9411473bc0225322512f6a
Jason He <omale@outlook.com>
2022-04-13 15:23:40 +1200

    fix meta data

--- pyscript/metadata.py
+++ pyscript/metadata.py
@@ -1,28 +1,26 @@
-import sys
+"""
+Convert image path from pellican directory to jeklly 
+"""
+import os
+import pathlib
 
-filename = sys.argv[1]
-print(filename)
-f = open(filename, "r").read()
-lines = f.splitlines()
-first, rest = lines[:10], lines[10:]
-fixed_first = ["---", "layout: post"]
 
-for line in first:
-    for key in ["Title:", "Date:", "Slug:"]:
-        if line.startswith(key):
-            line = line.replace(key, key.lower())
-            if key == "Date:":
-                if not filename.startswith("20"):
-                    date = line[6 : 6 + 10] + "-"
-                else:
-                    date = ""
-    if line.startswith("Author:"):
-        continue
-    if line.startswith("Tags:"):
-        line = line.replace("Tags: ", "categories: [") + "]"
-    fixed_first.append(line)
-    if line.startswith("slug:"):
-        fixed_first.append("---")
+def main():
+    # find root path
+    root_dir = pathlib.PosixPath(__file__).parent.parent
+    post_dir = pathlib.PosixPath(root_dir, '_posts')
+    img_dir = pathlib.PosixPath(root_dir, 'assets/images')
 
-with open(f"jekyll/{date}{filename}", "w") as f:
-    f.writelines(l + "\n" for l in fixed_first + rest)
+    for f in post_dir.iterdir():
+        if f.suffix.lower() != '.md':
+            print(f"{f} not markdown")
+            continue
+        # process markdown
+        text = f.read_text()
+        # process metadata
+        
+        f.write_text(text)
+
+main()
+
+
